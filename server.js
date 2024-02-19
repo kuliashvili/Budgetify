66eb71fe85223163033cecfffce7c5d69e6ad65a
giorgi <giorgi.kuliashvili.1@btu.edu.ge>
2024-02-19 15:45:30 +0400

    added login users from expressjs server, login usercases

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors'); // Import the cors package
+const express = require("express");
+const cors = require("cors"); // Import the cors package
 const app = express();
 const port = 3001;
+const session = require("express-session");
 
 app.use(cors()); // Enable CORS for all routes
+app.use(
+  session({
+    secret: "secret",
+    cookie: { maxAge: 60 * 60 * 1000 }, //1hr
+  })
+);
 
 const users = [
-  { id: 1, name: 'Gio', email: 'gio@example.com', password: '1234' },
-  { id: 2, name: 'Luka', email: 'luka@example.com', password: '1234' },
-  { id: 3, name: 'Vano', email: 'vano@example.com', password: '1234' }
+  { id: 1, name: "Gio", email: "gio@example.com", password: "1234" },
+  { id: 2, name: "Luka", email: "luka@example.com", password: "1234" },
+  { id: 3, name: "Vano", email: "vano@example.com", password: "1234" },
 ];
 
-app.get('/api/users', (req, res) => {
+app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -4,10 +4,13 @@ import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 
 function Login() {
+  let error = document.getElementById("error-field");
   const [counter, setCounter] = useState(0);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loggedIn, setLoggedIn] = useState(false);
+  const [emailError, setEmailError] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
 
   const handleLogin = async () => {
     try {
@@ -22,13 +25,23 @@ function Login() {
         alert("hello " + user.name + ", enjoy ");
         // window.location.href = "/main";
       } else {
-        alert("login or password is wrong, please try again");
+        // alert("login or password is wrong, please try again");
+        error.style.display = "flex"
       }
     } catch (error) {
       console.error("Error fetching users:", error);
       alert("An error occurred while logging in");
+      
     }
   };
+
+  const validateEmail = () => {
+    setEmailError( email == "")
+  }
+
+  const validatePassword = () => {
+    setPasswordError( password == "")
+  }
 
   function showPassword() {
     setCounter(counter + 1);
@@ -41,8 +54,11 @@ function Login() {
     }
   }
 
-  return (
+  function hideError(){
+    error.style.display = "none"
+  }
 
+  return (
     <div className="Login">
       {loggedIn ? (
         <Navigate to="/main" />
@@ -53,14 +69,21 @@ function Login() {
               <h1 className="login-header">Budgetify</h1>
             </div>
             <div className="login-down">
+              <div id="error-field" className="error-field" >
+                <p className="error-field-text" >Incorrect email or password</p>
+                <button onClick={hideError} className="error-close" >aaa</button>
+              </div>
               <div className="login-form">
                 <input
                   placeholder="Email"
                   name="email"
                   label="Email"
                   required
+                  onBlur={validateEmail}
                   onChange={(e) => setEmail(e.target.value)}
+                  className={emailError ? "error" : ""}
                 ></input>
+                {emailError && <div className="error-message" >Email is required</div>}
                 <div className="password-input-container">
                   <input
                     id="input-password"
@@ -68,13 +91,19 @@ function Login() {
                     name="password"
                     label="Password"
                     type="password"
-                    onChange={(e) => setPassword(e.target.value)}
                     required
+                    onBlur={validatePassword}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className={passwordError ? "error" : ""}
                   ></input>
                   <button onClick={showPassword} className="eye-button">
                     <img className="eye-icon" src={eyeIcon} alt="eyeIcon"></img>
                   </button>
                 </div>
+                {passwordError && <div className="error-message" >Password is required</div>}
+
+
+                
 
                 <button
                   className="login-button"
